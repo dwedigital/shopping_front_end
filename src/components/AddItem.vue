df782e4e5918729f5fd20168faa6c0a8f4fa1f3a
Dave Edwards <dave@dwedigital.com>
2020-10-25 09:35:30 +0000

    fixed input field issue

--- src/components/AddItem.vue
+++ src/components/AddItem.vue
@@ -1,33 +1,30 @@
 <template>
-    <b-modal ref="addItemModal"
-         id="item-modal"
-         title="Add a new item"
-         hide-footer>
-  <b-form @submit="onSubmit" @reset="onReset" class="w-100">
-  <b-form-group id="form-title-group"
-                label="Item:"
-                label-for="form-item-input">
-      <b-form-input id="form-item-input"
-                    type="text"
-                    v-model="addItemForm.item"
-                    required
-                    placeholder="Enter item">
-      </b-form-input>
-    </b-form-group>
-    <b-form-group id="form-quantity-group"
-                  label="Quantity:"
-                  label-for="form-quantity-input">
-        <b-form-input id="form-quantity-input"
-                      type="number"
-                      v-model="addItemForm.quantity"
-                      required
-                      placeholder="Enter quantity">
+  <b-modal ref="addItemModal" id="item-modal" title="Add a new item" hide-footer>
+    <b-form @submit="onSubmit" @reset="onReset" class="w-100">
+      <b-form-group id="form-title-group" label="Item:" label-for="form-item-input">
+        <b-form-input
+          id="form-item-input"
+          type="text"
+          v-model="addItemForm.item"
+          required
+          placeholder="Enter item"
+        >
         </b-form-input>
       </b-form-group>
-    <b-button type="submit" variant="primary">Submit</b-button>
-    <b-button type="reset" variant="danger">Reset</b-button>
-  </b-form>
-</b-modal>
+      <b-form-group id="form-quantity-group" label="Quantity:" label-for="form-quantity-input">
+        <b-form-input
+          id="form-quantity-input"
+          type="number"
+          v-model="addItemForm.quantity"
+          required
+          placeholder="Enter quantity"
+        >
+        </b-form-input>
+      </b-form-group>
+      <b-button type="submit" variant="primary">Submit</b-button>
+      <b-button type="reset" variant="danger">Reset</b-button>
+    </b-form>
+  </b-modal>
 </template>
 
 <script>
@@ -44,28 +41,6 @@ export default {
     };
   },
   methods: {
-    addItem(payload) {
-      const path = 'https://shopping-back-end.herokuapp.com/list';
-      axios.post(path, payload)
-        .then(() => {
-          this.$parent.getList();
-          this.$parent.alert({
-            message: 'Item added',
-            showMessage: true,
-          });
-        })
-        .catch((error) => {
-          // eslint-disable-next-line
-          console.log(error);
-          this.$.parent.getList();
-          this.showMessage = 'There was an error';
-          this.showMessage = true;
-          setTimeout(() => {
-            this.showMessage = false;
-          }, 2000);
-          console.log(error);
-        });
-    },
     onSubmit(evt) {
       evt.preventDefault();
       this.$refs.addItemModal.hide();
@@ -82,8 +57,31 @@ export default {
       this.initForm();
     },
     initForm() {
-      this.addItemForm.title = '';
+      this.addItemForm.item = '';
       this.addItemForm.quantity = 1;
+    },
+    addItem(payload) {
+      const path = 'https://shopping-back-end.herokuapp.com/list';
+      axios
+        .post(path, payload)
+        .then(() => {
+          this.$parent.getList();
+          this.$parent.alert({
+            message: 'Item added',
+            showMessage: true,
+          });
+        })
+        .catch((error) => {
+          // eslint-disable-next-line
+          console.log(error);
+          this.$.parent.getList();
+          this.showMessage = 'There was an error';
+          this.showMessage = true;
+          setTimeout(() => {
+            this.showMessage = false;
+          }, 2000);
+        });
+      this.initForm();
     },
   },
 };
